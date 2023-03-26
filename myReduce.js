ac62a4a7b0b7df2104711acc43a2c2243410264e
xdd <45672942@qq.com>
2023-03-27 01:41:24 +0800

    feat(myReduce)：实现myReduce方法

---
+++ myReduce.js
@@ -0,0 +1,18 @@
+Array.prototype.myReduce = function(callback, initialValue) {
+  const arr = this;
+  let accumulator = initialValue !== undefined? initialValue : arr[0];
+  for(let i = initialValue !== undefined? 0 : 1; i < arr.length; i ++) {
+    callback(accumulator, arr[i], i, arr);
+  } 
+  return accumulator;
+}
+
+//
+Array.prototype.myReduce = function(callback, initialValue) {
+let arr = this;
+let accumulator = initialValue !== undefined ? initialValue : arr[0];
+for (let i = initialValue !== undefined ? 0 : 1; i < arr.length; i++) {
+  accumulator = callback(accumulator, arr[i], i, arr);
+}
+return accumulator;
+};
